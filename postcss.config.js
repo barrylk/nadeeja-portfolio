b0864711a9e1457eda641df9d55eaa705fada230
barrylk <114392920+barrylk@users.noreply.github.com>
2025-04-23 16:50:46 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    '@tailwindcss/postcss', // Make sure this is here
-    'autoprefixer',
-  ],
-}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
